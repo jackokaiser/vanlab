35216c15fcc2d202e57304eb8ab1d30a5d0000cb
Jacques KAISER <jackokaiser@gmail.com>
2020-08-07 11:20:22 +0200

    Accurate contact page

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -4,30 +4,12 @@ import style from './style';
 const photographs = (props) => {
 	return (
 		<div class={style.pageContact}>
-			<h1 class={style.pageTitle}>Contact me</h1>
-			<div class={style.formWrapper}>
-				<p class={style.pageBody}>
-					<div>Hi!</div>
-					<div>If you are interested in my work and are looking to contact me for a contract please use the following form to contact me.</div>
-					<div>&nbsp;</div>
-					<div>Cheers 🍻</div>
-				</p>
-				<form name="contact" method="POST" data-netlify="true" action="/contact/success" data-netlify-honeypot="bot-field">
-					<input type="hidden" name="form-name" value="contact" />
-					<p>
-						<input type="text" name="name" placeholder="Name" required />
-					</p>
-					<p>
-						<input type="email" name="email" placeholder="E-Mail" required />
-					</p>
-					<p>
-						<textarea name="message" placeholder="Message" />
-					</p>
-					<p>
-						<button type="submit">Send</button>
-					</p>
-				</form>
-			</div>
+			<h1 class={style.pageTitle}>Contact</h1>
+			<h2>Who we are:</h2>
+			<ul>
+				<li>Camille</li>
+				<li>Jacques</li>
+			</ul>
 		</div>
 	);
 };
