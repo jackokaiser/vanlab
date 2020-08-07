1152948691d7bb2e7c98023c17cfe7425b3c72d4
Jacques KAISER <jackokaiser@gmail.com>
2020-08-07 13:22:18 +0200

    Crawling technical debt fix

---
+++ content/pages/contact.md
@@ -0,0 +1,6 @@
+---
+---
+
+## Who we are
+
+[Jacques](https://jacqueskaiser.com/) and [Camille](https://www.linkedin.com/in/camillesifferlen).
--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -1,17 +1,32 @@
 import { h } from 'preact';
 import style from './style';
+import { usePrerenderData } from '@preact/prerender-data-provider';
+import Markdown from 'markdown-to-jsx';
 
 const contact = (props) => {
-	return (
-		<div class={style.pageContact}>
-			<h1 class={style.pageTitle}>Contact</h1>
-			<h2>Who we are:</h2>
-			<ul>
-				<li>Camille</li>
-				<li>Jacques</li>
-			</ul>
-		</div>
-	);
+	const [data, isLoading] = usePrerenderData(props);
+
+	if (isLoading) {
+		return (
+			<div>
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
+
+	if (data && data.data) {
+		const { content, details } = data.data;
+		return (
+			<div>
+				<h1 class={style.pageTitle}>Contact</h1>
+				<div class="container-fluid mt-3">
+					<Markdown>
+						{ content }
+					</Markdown>
+				</div>
+			</div>
+		);
+	}
 };
 
 export default contact;
