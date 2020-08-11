3cd1cd01facc6946b03e3bd9946c24ffe048d71c
Jacques KAISER <jackokaiser@gmail.com>
2020-08-11 18:41:14 +0200

    whitespace

--- src/routes/notfound/index.js
+++ src/routes/notfound/index.js
@@ -4,13 +4,13 @@ import { Link } from 'preact-router/match';
 
 const Notfound = () => {
 
-    return (
-        <div class={style.notfound}>
-            <h1>Error 404</h1>
-            <p>That page doesn't exist.</p>
-            <Link href="/"><h4>Back to Home</h4></Link>
-        </div>
-    )
+	return (
+		<div class={style.notfound}>
+			<h1>Error 404</h1>
+			<p>That page doesn't exist.</p>
+			<Link href="/"><h4>Back to Home</h4></Link>
+		</div>
+	)
 }
 
 export default Notfound;
